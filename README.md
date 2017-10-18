6d351a9f4f0f67abf837d5c4c8de1df32e2f0111
zuhito <zuhito@hotmail.co.jp>
2017-10-18 19:26:48 +0900

    Support other classifiers

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # node-red-contrib-opencv
 Node-RED node for OpenCV
 
-Image file buffer and file path are supported as input message.
-Currently output is only result of face detection.
+This node can detect face, eye, eyeglasses, fullbody and car from image using OpenCV.
+Image file buffer and file path are supported as input message. JSON (coordinate and size of object) and image buffer (original image and red circles of objects) can be selected as output message.
 
 # How to Install
 Only Ubuntu and Raspbian environments are supported.
 
 (1) Install OpenCV
-> sudo apt-get install libopencv-dev
+- sudo apt-get install libopencv-dev
 
 (2) Install OpenCV node
-> cd ~/.node-red
-> npm install node-red-contrib-opencv
+- cd ~/.node-red
+- npm install node-red-contrib-opencv
 
 (Node installation function on Node-RED palette is not recommended because it's difficult to see installation progress which takes long time like this node)
 
 (3) Run Node-RED
-> node-red
+- node-red
 
--- node.js
+++ node.js
@@ -6,13 +6,36 @@ module.exports = function(RED) {
         var node = this;
         node.on('input', function(msg) {
             cv.readImage(msg.payload, function(err, im){
-                im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+                var classifier;
+                if (config.detect === "FACE_CASCADE") {
+                    classifier = cv.FACE_CASCADE;
+                } else if (config.detect === "EYE_CASCADE") {
+                    classifier = cv.EYE_CASCADE;
+                } else if (config.detect === "EYEGLASSES_CASCADE") {
+                    classifier = cv.EYEGLASSES_CASCADE;
+                } else if (config.detect === "FULLBODY_CASCADE") {
+                    classifier = cv.FULLBODY_CASCADE;
+                } else if (config.detect === "CAR_SIDE_CASCADE") {
+                    classifier = cv.CAR_SIDE_CASCADE;
+                } else {
+                    node.error("Unsupported classifier: " + config.detect);
+                }
+                im.detectObject(classifier, {}, function(err, faces){
                     if (!err) {
-                        console.log(JSON.stringify(faces));
-                        msg.payload = faces;
-                        node.send(msg);
+                        if (config.output === "json") {
+                            msg.payload = faces;
+                            node.send(msg);
+                        } else if (config.output === "buffer") {
+                            for (var i=0;i<faces.length; i++){
+                                var x = faces[i]
+                                im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+                            }
+                            msg.payload = im.toBuffer();
+                            node.send(msg);
+                        } else {
+                            node.error("Unsupported output: " + config.output);
+                        }
                     } else {
-                        console.log("error: " + err);
                         node.error(err);
                     }
                 });
